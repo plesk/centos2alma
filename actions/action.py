e44c7962898b6257b05dbecd55a498baa0f52592
Mikhail Sandakov <msandakov@plesk.com>
2022-12-21 13:25:07 +0200

    Add the way to skip actions based on some action internal logic

--- actions/action.py
+++ actions/action.py
@@ -22,6 +22,13 @@ class Action():
         except Exception as ex:
             raise Exception("Finishing action '{name}' has been failed".format(name=self.name)) from ex
 
+    def is_required(self):
+        return self._is_required()
+
+    def _is_required(self):
+        # All actions are required by default - just to simplefy things
+        return True
+
     def _prepare_action(self):
         raise NotImplementedError("Not implemented prapare action is called")
 
@@ -50,7 +57,11 @@ class ActionsFlow():
             self._pre_stage(stage_id, actions)
             for action in actions:
                 print("Making {description!s}".format(description=action))
+
+                if not self._is_action_required(action):
+                    continue
                 self._invoke_action(action)
+
                 print("{feel}OK".format(feel="." * (40 - len(str(action)))))
             self._post_stage(stage_id, actions)
 
@@ -63,6 +74,9 @@ class ActionsFlow():
 
     def _post_stage(self, stage_id, actions):
         pass
+
+    def _is_action_required(self, action):
+        return action.is_required()
 
     def _invoke_action(self, action):
         pass
