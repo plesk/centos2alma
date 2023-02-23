aaaaebe4606d1260b69c389c7007890cf2e1af2d
Mikhail Sandakov <msandakov@plesk.com>
2023-02-23 16:32:18 +0200

    Add information about status and monitor arguments into pre-reboot message

--- common/messages.py
+++ common/messages.py
@@ -5,6 +5,10 @@ CONVERT_RESTART_MESSAGE = """
 The preparation process is over. The system will be rebooted now to perform the conversion inside a temporary upgrade distro.
 You cannot connect to the instance via ssh during this process. To monitor the process, you could use a console port.
 The conversion process will takes about 25 minutes. Current server time: {}.
+When you able to connect to the instance again, you could use the following command to check the conversion status:
+    distupgrader --status
+or
+    distupgrader --monitor
 **************************************************************************************\033[0m
 """
 
