d044a64b9c18280f80daf423ab98b11cc2b32d3d
Mikhail Sandakov <msandakov@plesk.com>
2023-03-17 15:58:15 +0200

    Make clean leapp parts after conversion finished

--- actions/installation.py
+++ actions/installation.py
@@ -1,28 +1,46 @@
 from .action import ActiveAction
 
-import common
-from common import util
+import os
+import shutil
+
+from common import rpm, util
 
 
 class LeapInstallation(ActiveAction):
 
     def __init__(self):
         self.name = "installing leapp"
-
-    def _prepare_action(self):
-        if not common.is_package_installed("elevate-release"):
-            util.logged_check_call(["yum", "install", "-y", "http://repo.almalinux.org/elevate/elevate-release-latest-el7.noarch.rpm"])
-
-        pkgs_to_install = [
+        self.pkgs_to_install = [
+            "leapp",
+            "python2-leapp",
             "leapp-upgrade",
             "leapp-data-almalinux",
         ]
 
-        util.logged_check_call(["yum", "install", "-y"] + pkgs_to_install)
+    def _prepare_action(self):
+        if not rpm.is_package_installed("elevate-release"):
+            util.logged_check_call(["yum", "install", "-y", "http://repo.almalinux.org/elevate/elevate-release-latest-el7.noarch.rpm"])
+
+        util.logged_check_call(["yum", "install", "-y"] + self.pkgs_to_install)
 
     def _post_action(self):
-        # Todo. We could actually remove installed leap packages at the end
-        pass
+        rpm.remove_packages(rpm.filter_installed_packages(self.pkgs_to_install + ["elevate-release"]))
+
+        leapp_related_files = [
+            "/root/tmp_leapp_py3/leapp",
+        ]
+        for file in leapp_related_files:
+            os.unlink(file)
+
+        leapp_related_directories = [
+            "/etc/leapp",
+            "/var/lib/leapp",
+            "/var/log/leapp",
+            "/usr/lib/python2.7/site-packages/leapp",
+        ]
+        for directory in leapp_related_directories:
+            if os.path.exists(directory):
+                shutil.rmtree(directory)
 
     def _revert_action(self):
         pass
