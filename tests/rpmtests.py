1b7620cf2a5405acafb51a1110848b4a8fdbda12
SandakovMM <mmsandakov@gmail.com>
2023-05-02 14:49:35 +0300

    Add tests for writing repository data into file

--- tests/rpmtests.py
+++ tests/rpmtests.py
@@ -173,3 +173,58 @@ gpgcheck=0
         rpm.remove_repositories(self.REPO_FILE_NAME, [lambda id, _1, baseurl, _3: id == "repo2" or baseurl == "http://repo3"])
         with open(self.REPO_FILE_NAME) as file:
             self.assertEqual(file.read(), expected_content)
+
+
+class WriteRepodataTests(unittest.TestCase):
+    REPO_FILE_CONTENT = """[repo1]
+name=repo1
+baseurl=http://repo1
+enabled=1
+gpgcheck=0
+
+"""
+
+    REPO_FILE_NAME = "repo_file.txt"
+
+    def setUp(self):
+        with open(self.REPO_FILE_NAME, "w") as f:
+            f.write(self.REPO_FILE_CONTENT)
+
+    def tearDown(self):
+        if os.path.exists(self.REPO_FILE_NAME):
+            os.remove(self.REPO_FILE_NAME)
+
+    def test_write_repodata(self):
+        expected_content = """[repo1]
+name=repo1
+baseurl=http://repo1
+enabled=1
+gpgcheck=0
+
+[repo2]
+name=repo2
+baseurl=http://repo2
+enabled=1
+gpgcheck=0
+"""
+        rpm.write_repodata(self.REPO_FILE_NAME, "repo2", "repo2", "http://repo2", None, ["enabled=1\n", "gpgcheck=0\n"])
+        with open(self.REPO_FILE_NAME) as file:
+            self.assertEqual(file.read(), expected_content)
+
+    # Yeah, we don't check if reposiory is already in file. Maybe we will add the check in the future.
+    def test_write_exsisted_repodata(self):
+        expected_content = """[repo1]
+name=repo1
+baseurl=http://repo1
+enabled=1
+gpgcheck=0
+
+[repo1]
+name=repo1
+baseurl=http://repo1
+enabled=1
+gpgcheck=0
+"""
+        rpm.write_repodata(self.REPO_FILE_NAME, "repo1", "repo1", "http://repo1", None, ["enabled=1\n", "gpgcheck=0\n"])
+        with open(self.REPO_FILE_NAME) as file:
+            self.assertEqual(file.read(), expected_content)
