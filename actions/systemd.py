b9f192a2835beb637b2f39586e9a25a9a142a529
Mikhail Sandakov <msandakov@plesk.com>
2024-03-19 14:02:30 +0200

    Ensure that the service can be started before attempting to start it
    Existing services may not be available if the required services do not exist

--- actions/systemd.py
+++ actions/systemd.py
@@ -27,7 +27,9 @@ class RulePleskRelatedServices(action.ActiveAction):
             "sw-cp-server.service",
             "sw-engine.service",
         ]
-        self.plesk_systemd_services = [service for service in plesk_known_systemd_services if systemd.is_service_exists(service)]
+        self.plesk_systemd_services = [
+            service for service in plesk_known_systemd_services if systemd.is_service_can_be_started(service)
+        ]
 
         # Oneshot services are special, so they shouldn't be started on revert or after conversion, just enabled
         self.oneshot_services = [
@@ -36,9 +38,9 @@ class RulePleskRelatedServices(action.ActiveAction):
 
         # We don't remove postfix service when remove it during qmail installation
         # so we should choose the right smtp service, otherwise they will conflict
-        if systemd.is_service_exists("qmail.service"):
+        if systemd.is_service_can_be_started("qmail.service"):
             self.plesk_systemd_services.append("qmail.service")
-        elif systemd.is_service_exists("postfix.service"):
+        elif systemd.is_service_can_be_started("postfix.service"):
             self.plesk_systemd_services.append("postfix.service")
 
     def _prepare_action(self):
